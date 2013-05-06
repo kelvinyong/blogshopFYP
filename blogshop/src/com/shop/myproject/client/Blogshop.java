ee578501b2b7f2ae1efdd3f732b8cbac4b04523e
Kelvin <Kelvinyong87@gmail.com>
2013-05-07 00:04:33 +0800

    test

--- blogshop/src/com/shop/myproject/client/Blogshop.java
+++ blogshop/src/com/shop/myproject/client/Blogshop.java
@@ -42,7 +42,7 @@ public class Blogshop implements EntryPoint {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
 		final TextBox pwdField = new TextBox();
-		nameField.setText("User");
+		nameField.setText("Username");
 		pwdField.setText("password");
 		final Label errorLabel = new Label();
 
